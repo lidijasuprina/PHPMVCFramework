1450b1938d7d721562c814d33220fe6a604beed6
lidijasuprina <lidija.suprina@creative-square.agency>
2023-04-22 15:39:08 +0200

    Migrations & Connection to Database, .env file & Application config

--- core/Application.php
+++ core/Application.php
@@ -12,19 +12,21 @@ namespace app\core;
 class Application 
 {
     public static string $ROOT_DIR;
+    public static Application $app;
     public Request $request;
     public Response $response;
     public Router $router;
     public Controller $controller;
-    public static Application $app;
+    public Database $db;
 
-    public function __construct($rootPath) 
+    public function __construct($rootPath, array $config) 
     {
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+        $this->db = new Database($config['db']);
     }
 
     public function run() 
---
+++ migrations/m0001_something.php
@@ -0,0 +1,13 @@
+<?php
+
+class m0001_something {
+    public function up()
+    {
+        echo "Applying migration";
+    }
+
+    public function down()
+    {
+        echo "Down migration";
+    }
+}
