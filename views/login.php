8d117c0a3cdc500e3d1f15ef0bf5bf4013ff1879
lidijasuprina <lidija.suprina@creative-square.agency>
2023-04-23 16:37:09 +0200

    Implement Login

--- views/login.php
+++ views/login.php
@@ -1,17 +1,11 @@
+<?php
+/** @var $model \app\models\User */
+?>
+
 <h1>Login</h1>
 
-<form action="" method="post">
-    <div class="mb-3">
-        <label class="form-label">Subject</label>
-        <input type="text" name="subject" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Email address</label>
-        <input type="email" name="email" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Body</label>
-        <textarea name="body" class="form-control"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php $form = \app\core\form\Form::begin('', "post") ?>
+    <?php echo $form->field($model, 'email') ?>
+    <?php echo $form->field($model, 'password')->passwordField() ?>
+    <button type="submit" class="btn btn-primary mt-3">Submit</button>
+<?php echo \app\core\form\Form::end() ?>
